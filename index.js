0479e23d88bf905bffc8886cd71857fd8fef79f9
Vincent Weevers <mail@vincentweevers.nl>
2019-10-21 14:22:28 +0200

    Test batch write and iterator next

--- index.js
+++ index.js
@@ -1,47 +1,77 @@
 'use strict'
 
-const fs = require('fs')
-const platform = require('os').platform()
 const av = process.argv[2]
 const bv = process.argv[3]
 
 if (av && bv) {
-  const a = load(av)
-  const b = load(bv, av === bv)
+  if (av === bv) {
+    console.error('Cannot compare version against itself')
+    process.exit(1)
+  }
 
-  open(b, bv)
+  load(av)
+  const b = load(bv)
+
+  test(b, bv)
 } else if (av) {
-  open(load(av), av)
+  test(load(av), av)
 } else {
   console.error('usage: node index.js <version a> [version b]')
   process.exit(1)
 }
 
-function load (version, copy) {
+function load (version) {
   const nr = version.replace(/[^\w]/g, '')
-  const id = `ld${nr}/build/Release/leveldown.node`
-  const src = require.resolve(id)
-  const dest = src.replace(/leveldown\.node$/, `ld${nr}.${copy ? 'b.' : ''}node`)
-
-  fs.copyFileSync(src, dest)
-  console.log('Load %s', dest)
-  return require(dest)
+  const id = `ld${nr}`
+  console.log('Load %s (as %s)', version, id)
+  return require(id)
 }
 
-function open (binding, version) {
+function test (leveldown, version) {
   console.log('Open %s', version)
 
-  const context = binding.db_init()
   const location = 'db/' + Date.now()
-  const options = { createIfMissing: true, errorIfExists: false }
+  const db = leveldown(location)
+  let it
 
-  let calls = 0
-  binding.db_open(context, location, options, function (err) {
-    if (calls++) throw new Error('Called too many times')
+  db.open(once(onopen))
+
+  function onopen (err) {
+    if (err) throw err
+
+    db.batch()
+      .put('key', 'value')
+      .write(once(onwrite))
+  }
+
+  function onwrite (err) {
+    if (err) throw err
+
+    it = db.iterator({ keyAsBuffer: false, valueAsBuffer: false })
+    it.next(once(onnext))
+  }
+
+  function onnext (err, key, value) {
+    if (err) throw err
+    if (key !== 'key') throw new Error('Wrong key')
+    if (value !== 'value') throw new Error('Wrong value')
+
+    it.end(once(onend))
+  }
+
+  function onend (err) {
     if (err) throw err
 
     setTimeout(function () {
       console.log('\nOK')
     }, 1e3)
-  })
+  }
+}
+
+function once (fn) {
+  let calls = 0
+  return function () {
+    if (calls++) throw new Error('Called too many times')
+    fn.apply(this, arguments)
+  }
 }
